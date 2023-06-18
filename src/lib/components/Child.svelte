ef251129aebde5dd3d8bc17ed33ad59f56bfabf6
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-06-18 12:59:28 +0900

    pageを親として子コンポーネントをレンダー

---
+++ src/lib/components/Child.svelte
@@ -0,0 +1,17 @@
+<script>
+  /**
+  * @desc stringをprops
+  * @type {string}
+  **/
+  export let text
+
+  /**
+  * @desc brタグなどhtmlをprops
+  * @type {string}
+  **/
+  export let html
+</script>
+
+<p class="text">{text}</p>
+
+<p class="html">{@html html}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>こんにちは！svelte sample app</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Child from "$lib/components/Child.svelte"
+  let message = "world"
+  const prefix = "Svelte"
+
+  let text = "親コンポーネントから"
+  let html = "1<br>2<br>3"
+</script>
+
+<h1>Hello, {message} from {prefix}</h1>
+<Child {text} {html} />
+<style>
+  h1{
+    color:red;
+  }
+</style>
